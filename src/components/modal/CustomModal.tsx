96a8d0bd248a3f7a876d7448ade1eb45438b88b4
JEONBYEONGJIN <qudwls5020@naver.com>
2025-02-05 06:26:27 +0900

    feat: modal 컴포넌트 완료

--- src/components/modal/CustomModal.tsx
+++ src/components/modal/CustomModal.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import "./Modal.css";
+import styles from "./CustomModal.style.module.scss";
 
 interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
+  isOpen: boolean; // 모달이 열려 있는지 여부
+  onClose: () => void; // 모달을 닫는 함수
+  children: React.ReactNode; // 모달 내부에 렌더링할 내용
 }
 
-const Modal = ({ isOpen, onClose, children }: ModalProps) => {
+export default function CustomModal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <button className="modal-close" onClick={onClose}>
-          &times;
-        </button>
+    <div className={styles.modalOverlay} onClick={onClose}>
+      {/* 모달 내용 */}
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
   );
-};
-
-export default Modal;
+}
