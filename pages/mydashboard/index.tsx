95a9226166728b6e24b314f1700028838031087e
nagyum <wildanimal19@naver.com>
2025-02-05 20:26:14 +0900

    feature:대시보드 ui 및 api get 처리'

--- pages/mydashboard/index.tsx
+++ pages/mydashboard/index.tsx
@@ -2,28 +2,68 @@ import NavBar from "@/src/components/nav/NavBar";
 import SideBar from "@/src/components/sidebar/SideBar";
 import styles from "./index.module.scss";
 import { getDashboard } from "@/src/api/api";
-
-export default function MyDashboardPage() {
-  return (
-    <div className={styles.contents}>
-      <SideBar />
-      <NavBar />
-      <div className={styles.content}>안에 내용 들어감</div>
-    </div>
-  );
-}
+import ListCard from "@/src/components/dashboardlist/ListCard";
+import Pagination from "@/src/components/pagination/Pagination";
+import Image from "next/image";
 
 export async function getServerSideProps() {
   try {
     const { dashboards = [] } = await getDashboard();
 
     return {
-      props: { dashboards }, // Pass data to the component
+      props: { dashboards },
     };
   } catch (error) {
     console.error("Failed to fetch dashboard:", error);
     return {
-      props: { dashboards: [] }, // Return an empty array on error
+      props: { dashboards: [] },
     };
   }
+}
+
+export default function MyDashboardPage({ dashboards }: { dashboards: any[] }) {
+  return (
+    <div className={styles.contents}>
+      <SideBar />
+      <NavBar />
+      <div className={styles.content}>
+        <div>
+          <div className={styles.dashboard}>
+            {/* 새로운 대시보드 */}
+            <ListCard className={styles.listCard}>
+              <div>새로운 대쉬보드</div>
+              <Image
+                src="/icons/chip.svg"
+                width={22}
+                height={22}
+                alt="chip.svg"
+                priority
+              />
+            </ListCard>
+            {/* 대시보드 리스트 */}
+            {dashboards.map((dashboard, index) => (
+              <ListCard key={index}>
+                <div
+                  className={styles.colorCircle}
+                  style={{ backgroundColor: dashboard.color }}
+                ></div>
+                <div>{dashboard.title}</div>{" "}
+                <Image
+                  src="/icons/arrow.svg"
+                  width={22}
+                  height={22}
+                  alt="arrow.svg"
+                  priority
+                />
+              </ListCard>
+            ))}
+          </div>
+          {/* 페이지네이션 버튼 */}
+          <div className={styles.pagination}>
+            <Pagination />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
