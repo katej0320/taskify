638f1690fb68c2bcecef677f80b1183c5bbba65c
nagyum <wildanimal19@naver.com>
2025-02-05 15:06:55 +0900

    feature: 대시보드 api 생성

--- pages/mydashboard/index.tsx
+++ pages/mydashboard/index.tsx
@@ -1,8 +1,9 @@
 import NavBar from "@/src/components/nav/NavBar";
 import SideBar from "@/src/components/sidebar/SideBar";
 import styles from "./index.module.scss";
+import { getDashboard } from "@/src/api/api";
 
-export default function Page() {
+export default function MyDashboardPage() {
   return (
     <div className={styles.contents}>
       <SideBar />
@@ -10,4 +11,19 @@ export default function Page() {
       <div className={styles.content}>안에 내용 들어감</div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    const { dashboards = [] } = await getDashboard();
+
+    return {
+      props: { dashboards }, // Pass data to the component
+    };
+  } catch (error) {
+    console.error("Failed to fetch dashboard:", error);
+    return {
+      props: { dashboards: [] }, // Return an empty array on error
+    };
+  }
 }
