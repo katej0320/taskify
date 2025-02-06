f52eba52d696ffeb6949288cf6f07a495f8770ec
Kate Jang <bobaejang1@gmail.com>
2025-02-06 19:19:10 +0900

    폰트 인스톨 후 globalcss수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "pretendard/dist/web/static/pretendard.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
