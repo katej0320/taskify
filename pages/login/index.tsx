943923b81b5f5963c982c422b33b8428261e5e38
Kate Jang <bobaejang1@gmail.com>
2025-02-07 15:16:37 +0900

    fix: 눈알아이콘 기능 구현 완 및 에러메세지 버그 수정

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -9,22 +9,34 @@ import style from "./index.module.scss";
 import passwordeye from "@/public/images/passwordeye.png";
 import passwordeyeopen from "@/public/images/passwordeyeopen.png";
 import CustomModal from "@/src/components/modal/CustomModal";
-
-
+// import {Icon} from 'react-icons-kit';
+// import { ic_visibility_off } from 'react-icons-kit/md/ic_visibility_off';
+// import { ic_visibility } from 'react-icons-kit/md/ic_visibility';
 
 export default function LoginPage() {
   const [values, setValues] = useState({
     email: "",
     password: "",
   });
-  const [errorMessage, setErrorMessage] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   const [isPasswordVisible, setIsPasswordVisible] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [password, setPassword] = useState("");
+  const [type, setType] = useState("password");
+  // const [icon, setIcon] = useState(ic_visibility_off);
   const router = useRouter();
 
-
+  //   const handleToggle = () => {
+  //     if (type==='password'){
+  //        setIcon(ic_visibility);
+  //        setType('text')
+  //     } else {
+  //        setIcon(ic_visibility_off)
+  //        setType('password')
+  //     }
+  //  }
 
   // 유저가 입력한 값의 상태 저장
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -35,42 +47,43 @@ export default function LoginPage() {
       [name]: value,
     }));
 
+    // 이메일 형식 검증 함수
+    const validateEmail = (email: string) => {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+    };
+
     // 이메일 형식 실시간 검증
     if (name === "email") {
       if (value === "") {
-        setErrorMessage(""); // 이메일이 빈 값이면 에러 메시지 초기화
+        setEmailErrorMessage(""); // 이메일이 빈 값이면 에러 메시지 초기화
       } else if (!validateEmail(value)) {
-        setErrorMessage("이메일 형식으로 입력해주세요");
+        setEmailErrorMessage("이메일 형식으로 입력해주세요");
       } else {
-        setErrorMessage("");
+        setEmailErrorMessage("");
       }
     }
 
-    // 비밀번호 길이 검증
     if (name === "password") {
       if (value === "") {
         setPasswordError("");
-      } else if  (value.length < 8)
+      } else if (value.length < 8) {
         setPasswordError("비밀번호는 8자 이상이어야 합니다");
       } else {
         setPasswordError("");
       }
-    
+    }
 
     //로그인 버튼 비활성화/활성화화
-    if (validateEmail(values.email) && values.password.length > 8) {
-      setIsButtonDisabled(false);
-    } else {
-      setIsButtonDisabled(true);
-    }
+    setIsButtonDisabled(!(validateEmail(value) && value.length >= 8));
+
+    //   if (validateEmail(values.email) && values.password.length >= 8) {
+    //     setIsButtonDisabled(false);
+    //   } else {
+    //     setIsButtonDisabled(true);
+    //   }
+    // }
   }
-
-  // 이메일 형식 검증 함수
-  const validateEmail = (email: string) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
@@ -79,25 +92,22 @@ export default function LoginPage() {
     // axios 리퀘스트 보내기
 
     try {
-        const response = await axios.post('/auth/login',
-             {email, password},
-           //이거 확인하기기
-             { withCredentials :true});
-        console.log("로그인 성공", response.data);
-        router.push("/dashboard"); 
-
-        const { token } = response.data;
-
-        sessionStorage.setItem('token', token);
-
-    } catch (error: any){
-        console.error("로그인 실패:", error.response?.data || error.message);
-        setIsModalOpen(true);
-            
+      const response = await axios.post(
+        "/auth/login",
+        { email, password },
+        //이거 확인하기기
+        { withCredentials: true }
+      );
+      console.log("로그인 성공", response.data);
+      const { token } = response.data;
+      sessionStorage.setItem("token", token);
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, 0);
+    } catch (error: any) {
+      console.error("로그인 실패:", error.response?.data || error.message);
+      setIsModalOpen(true);
     }
-
-  
-
   }
 
   function handleSignupClick() {
@@ -111,7 +121,6 @@ export default function LoginPage() {
   const passwordVisible = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
-
 
   return (
     <div className={style.container}>
@@ -127,46 +136,45 @@ export default function LoginPage() {
         <p className={style.tag}>이메일</p>
         <input
           placeholder="이메일을 입력해주세요"
-          className={`${style.input} ${errorMessage ? style.inputError : ""}`}
+          className={`${style.input} ${
+            emailErrorMessage ? style.inputError : ""
+          }`}
           name="email"
           type="email"
           onChange={handleChange}
           value={values.email}
         />
-        {errorMessage && <span className={style.error}>{errorMessage}</span>}
+        {emailErrorMessage && (
+          <span className={style.error}>{emailErrorMessage}</span>
+        )}
 
-        <p className={style.tag}>비밀번호</p>
-        <div className={style.passwordWrapper}>
-          <div className={style.passwordinputandtext}>
-            <input
-              className={`${style.passwordinput} ${
-                passwordError ? style.inputError : ""
-              }`}
-              placeholder="비밀번호를 입력해 주세요"
-              name="password"
-              onChange={handleChange}
-              value={values.password}
-              type={isPasswordVisible ? "password" : "text"}
-            />
-            {passwordError && <span className={style.error}>{passwordError}</span>}
-           </div>
-          
+      <p className={style.tag}>비밀번호</p>
+      <div className={style.passwordWrapper}>
+          <input
+            className={`${style.passwordinput} ${passwordError ? style.inputError : ""}`}
+            placeholder="비밀번호를 입력해 주세요"
+            name="password"
+            onChange={handleChange}
+            value={values.password}
+            type={isPasswordVisible ? "text" : "password"}
+          />
+         
+        <span onClick={passwordVisible} className={style.passwordimg}>
           <Image
-            onClick={passwordVisible}
-            className={!isPasswordVisible ? style.passwordeyeopen : style.passwordeye} 
-            src={!isPasswordVisible ? passwordeyeopen : passwordeye}
+            className={
+              isPasswordVisible ? style.passwordeyeopen : style.passwordeye
+            }
+            src={isPasswordVisible ? passwordeyeopen : passwordeye}
             alt="Toggle Password Visibility"
           />
-           
-        
-       </div>
+        </span>
+        <span className={`${style.passwordError} ${passwordError ? style.show : ""}`}>
+          {passwordError}
+        </span>
+      </div>
 
 
-        
-       
-
-     
-        {/* 로그인버튼튼 */}
+        {/* 로그인버튼 */}
         <button
           className={`${style.loginbutton} ${
             !isButtonDisabled ? style.buttonActivated : ""
@@ -175,7 +183,6 @@ export default function LoginPage() {
         >
           로그인
         </button>
-
 
         {/* 회원가입페이지로이동버튼튼 */}
         <p className={style.signuptext}>
@@ -187,7 +194,7 @@ export default function LoginPage() {
       </form>
 
       {/* 모달 컴포넌트 */}
-      <CustomModal isOpen={isModalOpen} onClose={()=> setIsModalOpen(false)}>
+      <CustomModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         <p>비밀번호가 일치하지 않습니다.</p>
         <button onClick={() => setIsModalOpen(false)}>확인</button>
       </CustomModal>
