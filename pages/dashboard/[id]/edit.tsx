19112fc2a76d41a63e1c4a5168147e6253d76c0a
Nerim <nerim5037@gmail.com>
2025-02-10 12:46:14 +0900

    chore: 하단 여백 CSS 수정

--- pages/dashboard/[id]/edit.tsx
+++ pages/dashboard/[id]/edit.tsx
@@ -11,7 +11,8 @@ import { EditProvider } from "@/src/contexts/EditDashbordProvider";
 // 공통 스타일이기 때문에 작업을 위한 임시 CSS 적용
 const Container = styled.div`
   height: calc(env(safe-area-inset-bottom) + 100vh);
-  padding: 20px;
+  min-height: max-content;
+  padding: 20px 20px 50px;
   background: #fafafa;
 `;
 
