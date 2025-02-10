9245fa31eb756b39c8211887803d8e26d6635eb6
Nerim <nerim5037@gmail.com>
2025-02-10 17:56:10 +0900

    chore: useEffect dipendency 추가

--- src/contexts/EditDashboardProvider.tsx
+++ src/contexts/EditDashboardProvider.tsx
@@ -74,7 +74,7 @@ export function EditProvider({
       getMembers();
       getInvitations();
     }
-  }, [dashboardId, invitePage]);
+  }, [dashboardId, memberPage, invitePage]);
 
   return (
     <EditContext.Provider
