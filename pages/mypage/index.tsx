2a5a5e89e338d46c7633984ede71497b8f943cad
iadorora <iadorora@gmail.com>
2025-02-12 21:46:09 +0900

    fix: 데이터 요청 코드 수정

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -2,8 +2,8 @@
 
 import React from "react";
 import useSWR from "swr";
-import { getUser } from "@/src/api/userApi"; // 수정된 getUser 함수
-import { User } from "@/src/types/users"; // User 타입 임포트
+import { getUser } from "@/src/api/userApi"; 
+import { User } from "@/src/types/users"; 
 import styles from "./mypage.module.scss";
 import NavBar from "@/src/components/nav/NavBar";
 import SideBar from "@/src/components/sidebar/SideBar";
@@ -13,14 +13,13 @@ export default function MyPage() {
   // useSWR로 사용자 데이터 요청
   const { data: user, error } = useSWR<User>("/users/me", getUser); // <User> 타입 지정
 
-  if (error) return <p>데이터를 불러오는 중 오류 발생 😥</p>;
-  if (!user) return <p>로딩 중...</p>;
+  if (error) return <p>데이터 오류 발생 </p>;
+  if (!user) return <p>로딩 중</p>;
 
   return (
     <div className={styles.global}>
       <NavBar />
       <SideBar />
-      {/* user 데이터를 ProfileSettings에 전달 */}
       <ProfileSettings user={user} />
     </div>
   );
