174eeb30619a8be4b945a40f7928b531c42d41f7
Kate Jang <bobaejang1@gmail.com>
2025-02-15 19:44:50 +0900

    feat: 프로필아이콘 호버 하면 드랍다운 구현 완

--- src/components/nav/dropdown/Dropdown.tsx
+++ src/components/nav/dropdown/Dropdown.tsx
@@ -1,30 +1,33 @@
 "use client";
 
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import styles from './Dropdown.module.scss'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import styles from "./Dropdown.module.scss";
 
+// interface DropdownProps {
+//   children?: React.ReactNode;
+// }
 
 export default function Dropdown() {
+  const router = useRouter();
 
-    const router = useRouter();
   const handleLogout = () => {
     sessionStorage.removeItem("accessToken");
     router.push("/");
-  }
+  };
 
   return (
-    <div className={styles['dropmenu-container']}>
-      <div className={styles['button-container']}>
-        <button className={styles['menu-button']} onClick={handleLogout}>
+    <div className={styles["dropmenu-container"]}>
+      <div className={styles["button-container"]}>
+        <button className={styles["menu-button"]} onClick={handleLogout}>
           로그아웃
         </button>
       </div>
-      <div className={styles['button-container']}>
-        <button className={styles['menu-button']}>
-          <Link href="/myPage">내 정보</Link>
-        </button>
+      <div className={styles["button-container"]}>
+        <Link href="/mypage" className={styles["menu-button"]}>
+          내 정보
+        </Link>
       </div>
     </div>
-  )
+  );
 }
