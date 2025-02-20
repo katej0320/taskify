fc788496c9930c373a331cfc4faaae5601763268
Nerim <nerim5037@gmail.com>
2025-02-20 17:54:23 +0900

    fix: 마이페이지 프로필 섹션 닉네임 및 이미지 업데이트 이슈 수정 & 모달 처리

--- src/components/mypage/profilecard.tsx
+++ src/components/mypage/profilecard.tsx
@@ -5,77 +5,74 @@ import AvatarUploader from "./avataruploader";
 import { updateProfile } from "@/src/api/userApi";
 import axiosInstance from "@/src/api/axios";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { CheckModal } from "../dashboard/edit/modal/Check";
 
 interface ProfileCardProps {
   email: string;
-  nickname: string;
-  setNickname: (nickname: string) => void;
   recentNickname: string;
   recentProfileImg: string | StaticImport | null;
 }
 
 export default function ProfileCard({
   email,
-  nickname,
-  setNickname,
   recentNickname,
   recentProfileImg,
 }: ProfileCardProps) {
-  const [reqImage, setReqImage] = useState<File | string>("");
-  const [profileImage, setProfileImage] = useState();
+  const [isModal, setIsModal] = useState<boolean>(false);
+  const isMessage = "변경이 완료 되었습니다.";
   const [isUpdate, setIsUpdate] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [reqImage, setReqImage] = useState<File | null | undefined>();
+  const [isProfileImage, setIsProfileImage] = useState();
   const [isNicknameValue, setIsNicknameValue] = useState(recentNickname);
   const [isThumbnail, setIsThumbnail] = useState(recentProfileImg);
   const [isDisabled, setIsDisabled] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
- 
 
   const handleNicknameInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setNickname(e.target.value);
     setIsNicknameValue(e.target.value);
   };
 
   const handleSave = async () => {
     setLoading(true);
-    setError(null);
+    setIsModal(true);
 
-    try {
-      const response = await axiosInstance.post(
-        "/users/me/image",
-        {
-          image: reqImage,
-        },
-        {
-          headers: {
-            "Content-Type": "multipart/form-data",
+    if (reqImage !== undefined) {
+      try {
+        const response = await axiosInstance.post(
+          "/users/me/image",
+          {
+            image: reqImage,
           },
-        }
-      );
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
 
-      const { data } = response;
-      setProfileImage(data.profileImageUrl);
-
-      setIsUpdate(true);
-    } catch (err) {
-      setError("프로필 업데이트에 실패했습니다.");
-    } finally {
-      setLoading(false);
+        const { data } = response;
+        setIsProfileImage(data.profileImageUrl);
+      } catch (err) {
+        console.error(err);
+      }
     }
+    setIsUpdate(true);
   };
 
   useEffect(() => {
     if (isUpdate) {
       try {
-        updateProfile(nickname, profileImage);
-        console.log("프로필 저장 완료", { nickname, profileImage });
-        alert("프로필이 업데이트되었습니다!");
+        updateProfile(isNicknameValue, isProfileImage);
+        console.log("프로필 저장 완료", {
+          isNicknameValue,
+          profileImage: isProfileImage,
+        });
       } catch (error) {
         console.error(error);
       } finally {
         setIsUpdate(false);
         setIsDisabled(true);
+        setLoading(false);
       }
     }
   }, [isUpdate]);
@@ -83,55 +80,64 @@ export default function ProfileCard({
   useEffect(() => {
     if (
       isNicknameValue !== "" &&
-      isThumbnail !== "" &&
-      recentProfileImg !== isThumbnail &&
-      recentNickname !== isNicknameValue
+      recentNickname !== isNicknameValue &&
+      isNicknameValue !== "" &&
+      isThumbnail !== ""
     ) {
       setIsDisabled(false);
     } else if (
       isNicknameValue === "" ||
       isThumbnail === "" ||
-      recentProfileImg === isThumbnail ||
-      recentNickname === isNicknameValue
+      (isNicknameValue !== "" && isThumbnail === "") ||
+      isNicknameValue === recentNickname
     ) {
       setIsDisabled(true);
     }
-  }, [recentNickname, isNicknameValue, recentProfileImg, isThumbnail]);
+  }, [isNicknameValue, isThumbnail]);
 
   return (
-    <div className={styles.profileCard}>
-      <h2>프로필</h2>
-      <div className={styles.profileInfo}>
-        <AvatarUploader
-          setReqImage={setReqImage}
-          isThumbnail={isThumbnail}
-          setIsThumbnail={setIsThumbnail}
+    <>
+      {isModal && (
+        <CheckModal
+          isModal={isModal}
+          setIsModal={setIsModal}
+          isMessage={isMessage}
         />
-        <div className={styles.profileInputs}>
-          <div className={styles.profileInputGroup}>
-            <label>이메일</label>
-            <input type="email" value={email} disabled />
-          </div>
+      )}
+      <div className={styles.profileCard}>
+        <h2>프로필</h2>
+        <div className={styles.profileInfo}>
+          <AvatarUploader
+            setReqImage={setReqImage}
+            isThumbnail={isThumbnail}
+            setIsThumbnail={setIsThumbnail}
+          />
+          <div className={styles.profileInputs}>
+            <div className={styles.profileInputGroup}>
+              <label>이메일</label>
+              <input type="email" value={email} disabled />
+            </div>
 
-          <div className={styles.profileInputGroup}>
-            <label>닉네임</label>
-            <input
-              type="text"
-              defaultValue={recentNickname}
-              onChange={(e) => {
-                handleNicknameInput(e);
-              }}
-            />
+            <div className={styles.profileInputGroup}>
+              <label>닉네임</label>
+              <input
+                type="text"
+                defaultValue={recentNickname}
+                onChange={(e) => {
+                  handleNicknameInput(e);
+                }}
+              />
+            </div>
           </div>
         </div>
+        <button
+          onClick={handleSave}
+          className={styles.saveButton}
+          disabled={isDisabled}
+        >
+          {loading ? "저장 중" : "저장"}
+        </button>
       </div>
-      <button
-        onClick={handleSave}
-        className={styles.saveButton}
-        disabled={isDisabled}
-      >
-        {loading ? "저장 중" : "저장"}
-      </button>
-    </div>
+    </>
   );
 }
